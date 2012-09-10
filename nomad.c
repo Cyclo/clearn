b0516d8502fdc5109f627c8abfae9740d95479ac
Cyclo <eddie@cyclo.cyclo.local>
2012-09-10 23:02:13 +0100

    simple pack header reading writing

--- nomad.c
+++ nomad.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define offsetof(st, m) \
+       ((size_t) ( (char *)&((st *)0)->m - (char *)0 ))
+
 struct nd_header{
   
   char filename[255];
@@ -51,6 +54,7 @@ void create_pack(nd_file *f)
   f->headers = NULL;
   f->magic[0] = 'N';
   f->magic[1] = 'D';
+  f->magic[2] = '\0';
 }
 
 nd_header *get_last_header(nd_file *f)
@@ -60,6 +64,7 @@ nd_header *get_last_header(nd_file *f)
   }
 
   nd_header *t = f->headers;
+
   while(t && (t->next != NULL)){
     t = t->next;
   }
@@ -86,8 +91,8 @@ long get_next_offset(nd_file *f)
 long get_file_size(char *path)
 {
   long size;
-
   FILE *fp;
+
   fp = fopen(path,"rb");
   fseek(fp,0,SEEK_END);
   size = ftell(fp);
@@ -106,7 +111,7 @@ void add_file(nd_file *f, char *path)
   nd_header *last = get_last_header(f);
 
   if(!h){
-    die("Out of memory");
+    die("out of memory");
   }
   
   if(last){
@@ -120,6 +125,9 @@ void add_file(nd_file *f, char *path)
   
   h->offset = get_next_offset(f);
   h->length = get_file_size(path);
+  h->next = NULL;
+
+  f->count++;
 }
 
 void free_pack(nd_file *f)
@@ -132,13 +140,104 @@ void free_pack(nd_file *f)
   nd_header *h = f->headers;
   nd_header *t = NULL;
 
-   do{ 
-    //printf("Freeing %s\n",h->filename);  
+  while(h != NULL){
     t = h->next;
-    free(h);
+    free(t);
     h = t;
-  }while(h && (h->next != NULL));
+  }
+}
 
+void write_pack(nd_file *f, char *path)
+{
+  FILE *fp;
+  fp = fopen(path,"wb");
+
+  if(!fp){
+    die("couldn't open file for writing");
+  }
+  
+  // write the header
+  // TODO: Learn to write part of structure
+  fwrite(f,sizeof(nd_file),1,fp);
+  
+  // Loop headers and write
+  nd_header *h = f->headers;
+
+  while(h != NULL){
+  
+    fwrite(h,1,sizeof(nd_header),fp);
+    h = h->next;
+  }
+
+  fclose(fp);
+}
+
+void read_pack(nd_file *f, char *path)
+{
+
+  FILE *fp;
+  fp = fopen(path,"rb");
+  if(!fp){
+    die("couldn't open file for reading");
+  }
+
+  fread(f,1,sizeof(struct nd_file),fp);
+  f->headers = NULL;
+  
+  if(strcmp(f->magic,"ND") == 0){
+    
+    printf("read_pack: magic confirmed: ND\n");
+  }else{
+    fprintf(stderr,"magic didn't match ND!=%s\n",f->magic);
+    goto cleanup;
+  }
+
+  printf("read_pack: pack contains %d files\n",f->count);
+
+  // Read the first header
+  int tcount = 0;
+  nd_header *h,*t;
+  h = t = NULL;
+
+  while(tcount < f->count){
+    if(h){
+      t = h;
+    }
+    h = malloc(sizeof(nd_header));
+    if(!h){
+      fclose(fp);
+      die("out of memory");
+    } 
+    
+    if(!f->headers){
+      f->headers = h;
+    }else{
+      t->next = h;
+    }
+
+    fread(h,1,sizeof(struct nd_header),fp);
+    tcount++;
+  }
+
+
+cleanup:
+  fclose(fp);
+}
+
+
+void dump_pack(nd_file *f)
+{
+  if(!f && !f->headers){
+    die("dump_pack: no headers or nd_file");
+  }
+
+  nd_header *h = f->headers;
+
+  printf("----HEAD DUMP----\n");
+  while(h != NULL){
+    printf("Filename:%s\nOffset:%d\nSize:%d\n----------\n",h->filename,h->offset,h->length);
+    h = h->next;
+  }
 }
 
 int main()
@@ -147,9 +246,15 @@ int main()
   nd_file file;
   create_pack(&file);
   add_file(&file,"a.out");
+  add_file(&file,"test.txt");
+  write_pack(&file,"out.p");
   free_pack(&file);
 
-  printf("%s\n",basename("/test/test/test.txt"));
+  nd_file x = {0};
+  read_pack(&x,"out.p");
+  dump_pack(&x);
+  free_pack(&x);
+
   return 0;
 }
 
